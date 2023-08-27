46b18c8ce6b791e31ad254a36d34bbdde3c9bdbb
vegalla <70249519+vegalla@users.noreply.github.com>
2023-08-26 20:52:35 -0400

    Fix double canvas by removing Strict Mode

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import TDIB01 from './Processing/thedotisblack01';
 function App() {
   return (
     <div className={styles.App}>
-      <div className={styles.Header}>
-      GENERATIVE CIRCLE
-      </div>
-      <TDIB01 />
-      <div className={styles.Description}>
-      adapted from <a href="https://www.youtube.com/@thedotisblack">thedotisblack</a><br/>
-      <a href="https://www.youtube.com/watch?v=UZoVBMgzULk&list=PLbLdd1fdNg5zPBMYYZLO2JVqMrBkK5Lux&index=5">Ep.1 Processing Tutorial | GENERATIVE CIRCLE with line and loop</a>
+      <div>
+        <div className={styles.Header}>
+          GENERATIVE CIRCLE
+        </div>
+        <TDIB01 />
+        <div className={styles.Description}>
+          adapted from <a href="https://www.youtube.com/@thedotisblack">thedotisblack</a><br />
+          <a href="https://www.youtube.com/watch?v=UZoVBMgzULk&list=PLbLdd1fdNg5zPBMYYZLO2JVqMrBkK5Lux&index=5">Ep.1 Processing Tutorial | GENERATIVE CIRCLE with line and loop</a>
+        </div>
       </div>
     </div>
   );
--- src/Processing/thedotisblack01.js
+++ src/Processing/thedotisblack01.js
@@ -5,57 +5,58 @@ import getRandomInt from '../Functions/getRandomInt';
 
 const TDIB01 = () => {
 
-    const myRef = React.createRef();
+    const p5Ref = React.createRef();
 
-    const Sketch = (p) => {
+    const Sketch = (p5) => {
 
-        p.setup = () => {
-            p.createCanvas(800, 800);
-            p.noLoop();
+        p5.setup = () => {
+            p5.createCanvas(800, 800);
+            p5.noLoop();
         }
 
-        p.draw = () => {
-            p.background(15, 20, 30)
+        p5.draw = () => {
+            p5.background(15, 20, 30)
 
-            for (let i = -100; i < p.width + 100; i += 200) {
-                for (let j = -100; j < p.height + 100; j += 200) {
-                    p.noStroke();
-                    p.fill(getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255));
-                    p.rectMode(p.CENTER);
-                    p.rect(i, j, 200, 200);
-                    p.push();
-                    p.translate(i, j);
-                    p.scale(0.28);
+            for (let i = -100; i < p5.width + 100; i += 200) {
+                for (let j = -100; j < p5.height + 100; j += 200) {
+                    p5.noStroke();
+                    p5.fill(getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255));
+                    p5.rectMode(p5.CENTER);
+                    p5.rect(i, j, 200, 200);
+                    p5.push();
+                    p5.translate(i, j);
+                    p5.scale(0.28);
                     for (let n = 0; n < 30; n++) {
-                        p.stroke(getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255));
+                        p5.stroke(getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255));
                         for (let a = 0; a < 360; a += 2) {
-                            let x = getRandomInt(50, 150);
-                            let xx = getRandomInt(150, 350);
-                            p.push();
-                            p.rotate(p.radians(a));
-                            p.strokeCap(p.SQUARE);
-                            p.strokeWeight(4);
-                            console.log(x);
-                            console.log(xx);
-                            p.line(x, 0, xx, 0)
-                            p.pop();
+                            let x1 = getRandomInt(50, 150);
+                            let x2 = getRandomInt(150, 350);
+                            p5.push();
+                            p5.rotate(p5.radians(a));
+                            p5.strokeCap(p5.SQUARE);
+                            p5.strokeWeight(4);
+                            p5.line(x1, 0, x2, 0)
+                            p5.pop();
                         }
                     }
-                    p.pop();
+                    p5.pop();
                 }
             }
-
         }
+
+        p5.mouseClicked = () => {
+            p5.redraw();
+        };
     }
 
     useEffect(() => {
-        const myP5 = new p5(Sketch, myRef.current);
+        const myP5 = new p5(Sketch, p5Ref.current);
         return myP5.remove;
     }, [])
 
 
     return (
-        <div ref={myRef} />
+        <div ref={p5Ref} />
     )
 
 }
