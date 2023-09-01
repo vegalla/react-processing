0f310f7da1e07e2fd3837741cffd23277a8d2f55
vegalla <70249519+vegalla@users.noreply.github.com>
2023-09-01 00:07:58 -0400

    Animate Gallery Home Display Items

--- src/Components/GALLERY_HOME_CONFIG.js
+++ src/Components/GALLERY_HOME_CONFIG.js
@@ -1,3 +1,4 @@
 export const TAGS = [
-    "thedotisblack // adaptation",
+    "thedotisblack // adaptation",,
+    "vince // original",
 ]
--- src/Components/GalleryHome.js
+++ src/Components/GalleryHome.js
@@ -14,48 +14,58 @@ import DisplayItem from '../UI/DisplayItem';
 import { TAGS } from "./GALLERY_HOME_CONFIG";
 
 const GalleryHome = () => {
-    console.log({TAGS})
+    console.log({ TAGS })
     return (
         <>
             <div className={styles.Header}>
-                <h1>generative art</h1>
+                <span>generative art</span>
             </div>
             <div className={styles.DisplayZone}>
-                <DisplayItem
-                    image={static07}
-                    path="/07"
-                    label="NERVOUS RECTANGLE"
-                    tags={TAGS[0]}
-                    date="2023/08/31"
-                />
-                <DisplayItem
-                    image={static06}
-                    path="/06"
-                    label="OPTICAL ILLUSION | ROTATE + SCALE"
-                    tags={TAGS[0]}
-                    date="2023/08/29"
-                />
-                <DisplayItem
-                    image={static05}
-                    path="/05"
-                    label="ROTATE + SCALE"
-                    tags={TAGS[0]}
-                    date="2023/08/28"
-                />
-                <DisplayItem
-                    image={static04}
-                    path="/04"
-                    label="GRID WITH WHILE LOOP AND COLOR"
-                    tags={TAGS[0]}
-                    date="2023/08/27"
-                />
-                <DisplayItem
-                    image={static03}
-                    path="/03"
-                    label="VERA MOLNAR ALGORITHM"
-                    tags={TAGS[0]}
-                    date="2023/08/26"
-                />
+                <div className={styles.Appear0}>
+                    <DisplayItem
+                        image={static07}
+                        path="/07"
+                        label="NERVOUS RECTANGLE"
+                        tags={TAGS[0]}
+                        date="2023/08/31"
+                    />
+                </div>
+                <div className={styles.Appear1}>
+                    <DisplayItem
+                        image={static06}
+                        path="/06"
+                        label="OPTICAL ILLUSION | ROTATE + SCALE"
+                        tags={TAGS[0]}
+                        date="2023/08/29"
+                    />
+                </div>
+                <div className={styles.Appear2}>
+                    <DisplayItem
+                        image={static05}
+                        path="/05"
+                        label="ROTATE + SCALE"
+                        tags={TAGS[0]}
+                        date="2023/08/28"
+                    />
+                </div>
+                <div className={styles.Appear3}>
+                    <DisplayItem
+                        image={static04}
+                        path="/04"
+                        label="GRID WITH WHILE LOOP AND COLOR"
+                        tags={TAGS[0]}
+                        date="2023/08/27"
+                    />
+                </div>
+                <div className={styles.Appear4}>
+                    <DisplayItem
+                        image={static03}
+                        path="/03"
+                        label="VERA MOLNAR ALGORITHM"
+                        tags={TAGS[0]}
+                        date="2023/08/26"
+                    />
+                </div>
                 <DisplayItem
                     image={static02}
                     path="/02"
@@ -70,7 +80,7 @@ const GalleryHome = () => {
                     tags={TAGS[0]}
                     date="2023/08/25"
                 />
-            </div>
+            </div >
         </>
     )
 };
